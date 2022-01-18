984f58eb712677ce6897854228cef74369881613
Dmitry <dvpodlas@gmail.com>
2022-01-19 00:16:28 +0200

    added a block that pops up over time

--- src/js/modules/slider.js
+++ src/js/modules/slider.js
@@ -14,6 +14,19 @@ export default class Slider {
         if (n < 1) {
             this.slideIndex = this.slides.length;
         }
+        try {
+            this.hanson.style.opacity = '0';
+
+            if (n === 3) {
+                this.hanson.classList.add('animated');
+                setTimeout(() => {
+                    this.hanson.style.opacity = '1';
+                    this.hanson.classList.add('sliderInUp');
+                }, 3000);
+            } else {
+                this.hanson.classList.remove('sliderInUp');
+            }
+        } catch (error) {}
 
         this.slides.forEach(slide => {
             slide.style.display = 'none';
@@ -27,6 +40,10 @@ export default class Slider {
     }
 
     render() {
+        try {
+            this.hanson = document.querySelector('.hanson');
+        } catch (error) {}
+
         this.btns.forEach(item => {
             item.addEventListener('click', () => {
                 this.plusSlides(1);
